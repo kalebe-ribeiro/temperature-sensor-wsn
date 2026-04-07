9754dbbd42f2d7887ec1f5f6e1defc32a95ef6ed
kalebe-ribeiro <kaleberibeirolacerda@gmail.com>
2026-04-07 20:06:33 -0300

    0.1.232

--- temperature-humidity-sensor/src/sensor.cpp
+++ temperature-humidity-sensor/src/sensor.cpp
@@ -7,11 +7,9 @@ AlertValues sensor_alert(float humidity, float temperature) {
     alertValues.alert_temperature = false;
 
     if (humidity > THRESHOLD_HUMIDITY){
-        Serial.printf("Humidity has exceeded the threshold! %.2f%%\t", humidity);
         alertValues.alert_humidity = true;
     }
     if (temperature > THRESHOLD_TEMPERATURE){
-        Serial.printf("Temperature has exceeded the threshold! %.2f°C\n", temperature);
         alertValues.alert_temperature = true;
     }
     return alertValues;
